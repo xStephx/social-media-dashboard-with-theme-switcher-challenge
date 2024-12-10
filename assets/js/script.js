ba6ed3e0f3a35a3580d48198fe239bc074e85468
xStephx <bojkovskistefan9@gmail.com>
2024-12-10 18:59:16 +0100

    added comments for script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
+// DOM element
 const toggle = document.getElementById('check')
 
+// Setting current theme
 const currentTheme = localStorage.getItem('theme') || 'dark'
 
+// Setting dark theme
 if (currentTheme === 'dark') {
   document.documentElement.classList.add('dark')
   toggle.checked = true
 }
 
+// Event listener for changing themes
 toggle.addEventListener('change', () => {
   if (toggle.checked) {
     document.documentElement.classList.add('dark')
